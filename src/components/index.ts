daaef04f5cd84ce5680c61dd344589ca3d43742f
David Henery <david4henery00@hotmail.com>
2026-06-19 15:06:19 +0100

    added-basic-sections-&-layout-grid

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+import AboutSection from "./AboutSection.astro";
+import SliderSection from "./SliderSection.astro";
+import CTA from "./CTA.astro";
+
+export {
+  AboutSection,
+  CTA,
+  SliderSection
+}
